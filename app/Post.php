fbcd1fc907072f8303d3c4b88e3fbe53a66167b1
JMarze <arze.jesus@gmail.com>
2016-12-16 20:06:04 -0400

    Eliminación Lógica

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,14 @@
 namespace Blog;
 
 use Illuminate\Database\Eloquent\Model;
+// Eliminación lógica (1)
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    // Eliminación lógica (2)
+    use SoftDeletes;
+
     protected $table = 'posts';
 
     protected $fillable = ['codigo', 'titulo', 'contenido', 'publicado', 'categoria_id'];
