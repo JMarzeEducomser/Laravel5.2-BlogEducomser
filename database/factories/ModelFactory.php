64ffe487714b4175029bf1f3346721856db2b957
JMarze <arze.jesus@gmail.com>
2016-12-19 20:15:12 -0400

    Seeders y Model Factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,19 @@ $factory->define(Blog\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(Blog\Post::class, function (Faker\Generator $faker) {
+    return [
+        'codigo' => str_random(10),
+        'titulo' => $faker->sentence(6, true),
+        'contenido' => $faker->realText(300, 4),
+        'publicado' => $faker->boolean(70),
+        'categoria_id' => factory(Blog\Categoria::class)->create()->id,
+    ];
+});
+
+$factory->define(Blog\Categoria::class, function (Faker\Generator $faker) {
+    return [
+        'nombre' => $faker->sentence(1, true),
+    ];
+});
---
+++ database/seeds/CategoriasTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categorias')->insert([
+            'nombre' => 'Deportes',
+        ]);
+
+        DB::table('categorias')->insert([
+            'nombre' => 'Noticias',
+        ]);
+
+        DB::table('categorias')->insert([
+            'nombre' => 'Anuncios',
+        ]);
+    }
+}
