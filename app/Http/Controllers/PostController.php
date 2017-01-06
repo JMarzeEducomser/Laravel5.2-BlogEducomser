f43cde3f2f8330fa0cf8449256dd5746a0e45598
JMarze <arze.jesus@gmail.com>
2017-01-05 20:30:27 -0400

    CRUD Post (store)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -52,6 +52,11 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //dd($request->all());
+        $post = new Post($request->all());
+        //$post->codigo = $request['codigo'];
+        $post->save();
+
+        return redirect()->route('admin.post.index');
     }
 
     /**
